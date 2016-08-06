69b8333edfab2dfb917060474faca1c5b0f641fc
mcshekhar <mcshekhar2280@outlook.com>
2016-08-06 17:07:11 +0530

    Initialized the CMS project and modified Login, Log Out and Dashboard pages

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,44 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $scope.alerts = [
+			{
+				icon: 'glyphicon-user',
+				colour: 'btn-success',
+				total: '20,480',
+				description: 'TOTAL CUSTOMERS'
+			},
+			{
+				icon: 'glyphicon-eye-open',
+				colour: 'btn-warning',
+				total: '850',
+				description: 'FOLLOW UP REQUIRED'
+			},
+			{
+				icon: 'glyphicon-edit',
+				colour: 'btn-success',
+				total: '560',
+				description: 'NEW CUSTOMERS IN 2016'
+			},
+			{
+				icon: 'glyphicon-record',
+				colour: 'btn-info',
+				total: '2000',
+				description: 'EMAILS SENT'
+			},
+			{
+				icon: 'glyphicon-calendar',
+				colour: 'btn-primary',
+				total: '180',
+				description: 'UPCOMING EVENTS'
+			},
+			{
+				icon: 'glyphicon-flag',
+				colour: 'btn-danger',
+				total: '680',
+				description: 'REFERRALS TO MODERATE'
+			}
+		];
   }
 ]);
